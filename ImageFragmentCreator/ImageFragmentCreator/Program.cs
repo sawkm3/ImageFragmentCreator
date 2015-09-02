b47220b10a330e147a90b679da321ac63df188cc
sawkm3 <sawkm3@gmail.com>
2015-09-02 17:42:27 +0900

    READMEの更新

--- ImageFragmentCreator/ImageFragmentCreator/Program.cs
+++ ImageFragmentCreator/ImageFragmentCreator/Program.cs
@@ -15,7 +15,8 @@ namespace ImageFragmentCreator
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Quiz());
+            Application.Run(new Form1());
+            //Application.Run(new Quiz());
         }
     }
 }
